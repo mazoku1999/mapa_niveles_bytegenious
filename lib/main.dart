256a60c0496f617c03514861504b21ac938086eb
mazoku1999 <rogelio.carata10@gmail.com>
2024-08-07 14:31:40 -0400

    Se mejoro los mapas, ya son dinamicos y se ven mejor, version 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,36 +22,102 @@ class LevelMapScreen extends StatefulWidget {
 
 class _LevelMapScreenState extends State<LevelMapScreen> {
   final List<LevelNode> nodes = [
-    LevelNode(
-        offset: const Offset(210, 100), isUnlocked: true, isCompleted: true),
-    LevelNode(
-        offset: const Offset(260, 180), isUnlocked: true, isCompleted: true),
-    LevelNode(
-        offset: const Offset(310, 260), isUnlocked: true, isCompleted: false),
-    LevelNode(
-        offset: const Offset(260, 340), isUnlocked: false, isCompleted: false),
-    LevelNode(
-        offset: const Offset(210, 420), isUnlocked: false, isCompleted: false),
-    LevelNode(
-        offset: const Offset(160, 500), isUnlocked: false, isCompleted: false),
-    LevelNode(
-        offset: const Offset(110, 580), isUnlocked: false, isCompleted: false),
-    LevelNode(
-        offset: const Offset(160, 660), isUnlocked: false, isCompleted: false),
-    LevelNode(
-        offset: const Offset(210, 740), isUnlocked: false, isCompleted: false),
-    LevelNode(
-        offset: const Offset(260, 820), isUnlocked: false, isCompleted: false),
-    LevelNode(
-        offset: const Offset(310, 900), isUnlocked: false, isCompleted: false),
-    LevelNode(
-        offset: const Offset(260, 980),
-        isFinishedUnlocked: false,
-        isCompleted: false),
+    // LevelNode(
+    //     offset: const Offset(210, 100), isUnlocked: true, isCompleted: true),
+    // LevelNode(
+    //     offset: const Offset(260, 180), isUnlocked: true, isCompleted: true),
+    // LevelNode(
+    //     offset: const Offset(310, 260), isUnlocked: true, isCompleted: false),
+    // LevelNode(
+    //     offset: const Offset(260, 340), isUnlocked: false, isCompleted: false),
+    // LevelNode(
+    //     offset: const Offset(210, 420), isUnlocked: false, isCompleted: false),
+    // LevelNode(
+    //     offset: const Offset(160, 500), isUnlocked: false, isCompleted: false),
+    // LevelNode(
+    //     offset: const Offset(110, 580), isUnlocked: false, isCompleted: false),
+    // LevelNode(
+    //     offset: const Offset(160, 660), isUnlocked: false, isCompleted: false),
+    // LevelNode(
+    //     offset: const Offset(210, 740), isUnlocked: false, isCompleted: false),
+    // LevelNode(
+    //     offset: const Offset(260, 820), isUnlocked: false, isCompleted: false),
+    // LevelNode(
+    //     offset: const Offset(310, 900), isUnlocked: false, isCompleted: false),
+    // LevelNode(
+    //     offset: const Offset(260, 980),
+    //     isFinishedUnlocked: false,
+    //     isCompleted: false),
   ];
+
+  void cargarLevelNodes(int cantidadNiveles) {
+    for (int i = 0; i < cantidadNiveles; i++) {
+      int group = (i ~/ 4) % 2;
+
+      if (group == 0) {
+        // Izquierda
+        nodes.add(
+          LevelNode(
+            offset: Offset(230 - (i % 4) * 25, 90 + (i * 80)),
+            isUnlocked: i < 3,
+            isCompleted: i < 2,
+          ),
+        );
+      } else {
+        // Derecha
+        nodes.add(
+          LevelNode(
+            offset: Offset(150 + (i % 4) * 25, 90 + (i * 80)),
+            isUnlocked: i < 3,
+            isCompleted: i < 2,
+          ),
+        );
+      }
+    }
+  }
+
+  //los offsets deben ser como los de arriba dx de 50 en 50 y dy de 80 en 80, todo dinamicamente
+  // void cargarLevelNodes(int cantidadNiveles) {
+  //   for (int i = 0; i < cantidadNiveles; i++) {
+  //     int group;
+  //     if (i < 4) {
+  //       group = 0; // O cualquier valor que desees para los primeros tres nodos
+  //     } else {
+  //       group = ((i - 3) ~/ 4) % 2;
+  //     }
+
+  //     if (group == 0) {
+  //       // Izquierda
+  //       nodes.add(
+  //         LevelNode(
+  //           offset: Offset(210 - ((i - 3) % 4) * 15, 90 + (i * 80)),
+  //           isUnlocked: false,
+  //           isCompleted: false,
+  //         ),
+  //       );
+  //     } else {
+  //       // Derecha
+  //       nodes.add(
+  //         LevelNode(
+  //           offset: Offset(170 + ((i - 3) % 4) * 15, 90 + (i * 80)),
+  //           isUnlocked: false,
+  //           isCompleted: false,
+  //         ),
+  //       );
+  //     }
+  //   }
+  // }
+
+  @override
+  void initState() {
+    cargarLevelNodes(7);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final double maxHeight =
+        nodes.map((node) => node.offset!.dy).reduce((a, b) => max(a, b)) + 80;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Niveles del juego'),
@@ -57,7 +125,8 @@ class _LevelMapScreenState extends State<LevelMapScreen> {
       body: SingleChildScrollView(
         child: SizedBox(
           width: double.infinity,
-          height: 1080, // Aumentar el tamaño del height para permitir el scroll
+          height:
+              maxHeight, // Aumentar el tamaño del height para permitir el scroll
           child: Stack(
             children: [
               CustomPaint(
@@ -65,7 +134,7 @@ class _LevelMapScreenState extends State<LevelMapScreen> {
                     double.infinity, 1080), // Ajustar tamaño del CustomPaint
                 painter: LevelMapPainter(nodes),
               ),
-              ...nodes.map((node) => LevelNodeWidget(node: node)).toList(),
+              ...nodes.map((node) => LevelNodeWidget(node: node)),
             ],
           ),
         ),
@@ -82,8 +151,8 @@ class LevelNodeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      left: node.offset.dx - 20,
-      top: node.offset.dy - 20,
+      left: node.offset!.dx - 20,
+      top: node.offset!.dy - 20,
       child: GestureDetector(
         onTap: node.isUnlocked ? () => print('Node tapped') : null,
         child: CircleAvatar(
@@ -127,13 +196,13 @@ class LevelNodeWidget extends StatelessWidget {
 }
 
 class LevelNode {
-  final Offset offset;
+  final Offset? offset;
   final bool isUnlocked;
   final bool isCompleted;
   final bool? isFinishedUnlocked;
 
   LevelNode({
-    required this.offset,
+    this.offset,
     this.isUnlocked = false,
     this.isCompleted = false,
     this.isFinishedUnlocked,
@@ -152,7 +221,8 @@ class LevelMapPainter extends CustomPainter {
       ..strokeWidth = 2.0;
 
     for (int i = 0; i < nodes.length - 1; i++) {
-      canvas.drawLine(nodes[i].offset, nodes[i + 1].offset, paint);
+      canvas.drawLine(nodes[i].offset ?? Offset(0, 0),
+          nodes[i + 1].offset ?? Offset(0, 0), paint);
     }
   }
 
